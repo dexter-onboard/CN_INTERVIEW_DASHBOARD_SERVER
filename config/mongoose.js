adbe3706d64a95fa9f6de292db99d013d1f25f8c
Sudhendra <contactwithsid@gmail.com>
2023-12-01 20:47:16 +0530

    Added mongo atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 
-const mongoDB = mongoose.connect('mongodb://127.0.0.1:27017/interview-dashboard')
+// const mongoDB = mongoose.connect('mongodb://127.0.0.1:27017/interview-dashboard')
+const mongoDB = mongoose.connect(process.env.MONGODB_ATLAS_URL)
 
 const db = mongoose.connection
 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const mongoDB = require('./config/mongoose')
 const Admin = require('./model/admin')
 const app = express()
